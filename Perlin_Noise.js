c97d14a0b2e19685ac73b0e7d791ff1ddb4cfc5b
Liam Stone <stonediggity@gmail.com>
2022-03-30 17:09:28 +1100

    Update Perlin_Noise.js

--- Perlin_Noise.js
+++ Perlin_Noise.js
@@ -1,1 +1,29 @@
+// Initial plot of perlin noise in 2dimensions by iterating through x and y values and plotting out the relevant value of perlin noise in greyscale
+let inc = 0.1;
 
+function setup() {
+  createCanvas(400, 400);
+  pixelDensity(1);
+}
+
+function draw() {
+  let yoff = 0;
+  loadPixels();
+  for (let y = 0; y < height; y++) {
+    let xoff = 0;
+    for (let x = 0; x < width; x++) {
+      let index = (x + y * width) * 4;
+      // let r = random(255);
+      let r = noise(xoff, yoff) * (255);
+      pixels[index + 0] = r;
+      pixels[index + 1] = r;
+      pixels[index + 2] = r;
+      pixels[index + 3] = 255;
+
+      xoff += inc;
+    }
+    yoff += inc;
+  }
+  updatePixels();
+  //noLoop();
+}
